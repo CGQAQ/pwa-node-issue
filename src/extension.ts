ea9bcd1a5a6b7abe4dd8d9aa4172b2df0e15f329
Jason <m.jason.liu@outlook.com>
2020-09-17 12:44:38 +0800

    new issue: ERR_INVALID_ARG_TYPE from

--- src/extension.ts
+++ src/extension.ts
@@ -7,8 +7,17 @@ import * as vscode from 'vscode';
 export function activate(context: vscode.ExtensionContext) {
 	vscode.debug.registerDebugConfigurationProvider('deno', {
 		resolveDebugConfiguration(_, config) {
+      // "name": "Launch",
+      // "type": "pwa-node",
+      // "request": "launch",
+      // "program": "${workspaceFolder}/main.ts",
+      // "runtimeExecutable": "deno",
+      // "runtimeArgs": ["run", "--inspect-brk=0.0.0.0:7777", "-A"],
+      // "attachSimplePort": 7777
+      config.name = 'Launch';
 		  config.type = 'pwa-node';
-		  config.request= 'launch';
+      config.request= 'launch';
+      config.program = '${file}';
 		  return config;
 		}
 	});
